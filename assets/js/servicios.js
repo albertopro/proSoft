a394168c19b3179bb868dbba67de95b4284572b0
Alberto Mendoza <albertomvpro@gmail.com>
2019-08-06 17:38:14 -0500

    Cambios para el hover

--- assets/js/servicios.js
+++ assets/js/servicios.js
@@ -1,1 +1,8 @@
 //shadow-sm bg-white rounded
+/**
+ * Hover para efecto de los servicios
+ */
+$('.servicios').hover(function() {
+    $('.servicios').addClass('shadow-sm bg-white rounded');
+    $('.servicios').removeClass('shadow-sm bg-white rounded');
+});
